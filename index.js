fccf189f519260f0605779011e3e5471ce15cfab
Anatoly Litovsky <anatoly@nz1.ibm.com>
2018-05-28 11:34:48 +1200

    adding pull policy and switch to versioned containers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World\n');
+  res.send('Hello World version 1\n');
 });
 
 app.listen(PORT,"0.0.0.0")
