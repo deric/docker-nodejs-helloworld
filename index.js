cf8a71bd9cb594514a8c57748dd9aa62549b6c86
Anatoly Litovsky <anatoly@nz1.ibm.com>
2018-05-28 18:02:33 +1200

    Fixing more eslint errors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let express = require('express');
+const express = require('express');
 
 // Constants
 const DEFAULT_PORT = 8080;
-let PORT = process.env.PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || DEFAULT_PORT;
 
 // App
-let app = express();
-app.get('/', function (req, res) {
+const app = express();
+app.get('/', (req, res) => {
   res.send('Hello World version 1\n');
 });
 
-app.listen(PORT, "0.0.0.0")
-console.log('Running on http://0.0.0.0:' + PORT);
+app.listen(PORT, '0.0.0.0');
+console.log(`Running on http://0.0.0.0:${PORT}`);
