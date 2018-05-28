5f16ae1a0fa2e464084369fae7c0d9321e031224
Anatoly Litovsky <anatoly@nz1.ibm.com>
2018-05-29 11:00:57 +1200

    Some unit testing

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
-// Constants
-const DEFAULT_PORT = 8080;
-const PORT = process.env.PORT || DEFAULT_PORT;
+function server()
+{
+  // Constants
+  const DEFAULT_PORT = 8080;
+  const PORT = process.env.PORT || DEFAULT_PORT;
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World version 1\n');
-});
+  // App
+  const app = express();
+  app.get('/', (req, res) => {
+    res.send('Hello World version 1\n');
+  });
 
-app.listen(PORT, '0.0.0.0');
-console.log(`Running on http://0.0.0.0:${PORT}`);
+  app.listen(PORT, '0.0.0.0');
+  console.log(`Running on http://0.0.0.0:${PORT}`);
+}
+module.export = server;
+
+server()
---
+++ index.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('index,js')
+describe('Test the root path', () => {
+    test('It should response the GET method', (done) => {
+        request(app).get('/').then((response) => {
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toBe('Hello World version 1\n');
+            done();
+        });
+    });
+});
