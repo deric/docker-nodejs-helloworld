691849af70de9c2012f3ef1d2aaeded3379ec58e
Anatoly Litovsky <anatoly@nz1.ibm.com>
2018-06-01 16:35:01 +1200

    Fixing the name

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World Gary\n');
+  res.send('Hello World Garry\n');
 });
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ describe('Test the root path', () => {
   test('It should response the GET method', (done) => {
     request(app).get('/').then((response) => {
       expect(response.statusCode).toBe(200);
-      expect(response.text).toBe('Hello World version 1\n');
+      expect(response.text).toBe('Hello World Garry\n');
       done();
     });
   });
